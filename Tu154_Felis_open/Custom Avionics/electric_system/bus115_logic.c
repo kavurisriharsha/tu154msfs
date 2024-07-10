d02555d0251ea7519af1998e0c20005a8155fe4d
Sriharsha Kavuri <sriharshakavuri1@gmail.com>
2024-07-09 18:37:02 -0700

    bbus115 completed

--- Tu154_Felis_open/Custom Avionics/electric_system/bus115_logic.c
+++ Tu154_Felis_open/Custom Avionics/electric_system/bus115_logic.c
@@ -28,6 +28,262 @@ void update_115(elec_t* elec, acf_t* acf) {
             float gen2_volt = elec->gen2_volt;
             float gen3_volt = elec->gen3_volt;
             float gen4_volt = elec->gen4_volt;*/
+
+            if (gen1_work && gen2_work && gen3_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gen2_volt;
+                bus3_volt = elec->gen3_volt;
+
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp;
+                elec->gen2_amp = elec->bus115_2_amp;
+                elec->gen3_amp = elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen2_work && gen3_work) {
+                bus1_volt = elec->gen2_volt;
+                bus2_volt = elec->gen2_volt;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = elec->bus115_2_amp + elec->bus115_1_amp;
+                elec->gen3_amp = elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen1_work && gen3_work && gpu_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gpu_volt;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_2_amp;
+            }
+            else if (gen1_work && gen3_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gen1_volt;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp + elec->bus115_2_amp;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen1_work && gen2_work && gpu_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gen2_volt;
+                bus3_volt = elec->gpu_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp;
+                elec->gen2_amp = elec->bus115_2_amp;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_3_amp
+            }
+            else if (gen1_work && gen2_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gen2_volt;
+                bus3_volt = elec->gen2_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp;
+                elec->gen2_amp = elec->bus115_2_amp + elec->bus115_3_amp;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen1_work && gen4_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gen4_volt;
+                bus3_volt = elec->gen1_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = elec->bus115_2_amp;
+                elec->gpu_amp = 0;
+            }
+            else if (gen2_work && gen4_work) {
+                bus1_volt = elec->gen2_volt;
+                bus2_volt = elec->gen4_volt;
+                bus3_volt = elec->gen2_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = elec->bus115_2_amp;
+                elec->gpu_amp = 0;
+            }
+            else if (gen3_work && gen4_work) {
+                bus1_volt = elec->gen3_volt;
+                bus2_volt = elec->gen4_volt;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen4_amp = elec->bus115_2_amp;
+                elec->gpu_amp = 0;
+            }
+            else if (gen1_work && gpu_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = elec->gpu_volt;
+                bus3_volt = elec->gpu_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_2_amp + elec->bus115_3_amp;
+            }
+            else if (gen2_work && gpu_work) {
+                bus1_volt = elec->gpu_volt;
+                bus2_volt = elec->gen2_volt;
+                bus3_volt = elec->gpu_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = elec->bus115_2_amp;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+            }
+            else if (gen3_work && gpu_work) {
+                bus1_volt = elec->gpu_volt;
+                bus2_volt = elec->gpu_volt;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_1_amp + elec->bus115_2_amp;
+            }
+            else if (gen1_work) {
+                bus1_volt = elec->gen1_volt;
+                bus2_volt = 0;
+                bus3_volt = elec->gen1_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen2_work) {
+                bus1_volt = elec->gen2_volt;
+                bus2_volt = 0;
+                bus3_volt = elec->gen2_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen3_work) {
+                bus1_volt = elec->gen3_volt;
+                bus2_volt = 0;
+                bus3_volt = elec->gen3_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = elec->bus115_1_amp + elec->bus115_3_amp;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            else if (gen4_work + gpu_work) {
+                bus1_volt = elec->gen4_volt;
+                bus2_volt = elec->gen4_volt;
+                bus3_volt = elec->gpu_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = elec->bus115_1_amp + elec->bus115_2_amp;
+                elec->gpu_amp = elec->bus115_3_amp;
+            }
+            else if (gpu_work) {
+                bus1_volt = elec->gpu_volt;
+                bus2_volt = elec->gpu_volt;
+                bus3_volt = elec->gpu_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = elec->bus115_1_amp + elec->bus115_2_amp + elec->bus115_3_amp;
+            }
+            else if (gen4_work) {
+                bus1_volt = elec->gen4_volt;
+                bus2_volt = elec->gen4_volt;
+                bus3_volt = elec->gen4_volt;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = elec->bus115_1_amp + elec->bus115_2_amp + elec->bus115_3_amp;
+                elec->gpu_amp = 0;
+            }
+            else {
+                bus1_volt = 0;
+                bus2_volt = 0;
+                bus3_volt = 0;
+                bus1_em_volt = bus1_volt;
+                bus2_em_volt = bus3_volt;
+
+                elec->gen1_amp = 0;
+                elec->gen2_amp = 0;
+                elec->gen3_amp = 0;
+                elec->gen4_amp = 0;
+                elec->gpu_amp = 0;
+            }
+            
+            elec->bus115_1_volt = bus1_volt;
+            elec->bus115_2_volt = bus2_volt;
+            elec->bus115_3_volt = bus3_volt;
+
+            elec->bus115_em_1_volt = bus1_em_volt;
+            elec->bus115_em_2_volt = bus2_em_volt;
         }
     }
 }
