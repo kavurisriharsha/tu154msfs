c795fc22b3b4b5c2b8161fd2579b503568bce668
Sriharsha Kavuri <sriharshakavuri1@gmail.com>
2024-08-22 16:59:15 -0700

    hydro logic

--- Tu154_Felis_open/Custom Avionics/sim.h
+++ Tu154_Felis_open/Custom Avionics/sim.h
@@ -1,7 +1,4 @@
 typedef struct {
-    float ENGN_N1_0;
-    float ENGN_N1_1;
-    float ENGN_N1_2;
     int avionics_power_on;
     int rel_genera0;
     int rel_genera1;
@@ -11,7 +8,15 @@ typedef struct {
     int generator_on2;
     int APU_generator_on;
 
+    int parkbrake;
+
+
     int view_is_external;
+
+    float ENGN_N1_0;
+    float ENGN_N1_1;
+    float ENGN_N1_2;
+
     float groundspeed;
     float local_x;
     float local_y;
@@ -20,4 +25,29 @@ typedef struct {
     float view_x;
     float view_y;
     float view_z;
+
+    float wing1l_fla1def;
+    float wing1r_fla1def;
+
+
+    float wing3l_ail1def;
+    float wing3r_ail1def;
+
+    float wing1l_spo1def;
+    float wing1r_spo1def;
+
+    float wing2l_spo2def;
+    float wing2r_spo2def;
+
+    float hstab1_elv1def;
+    float hstab2_elv1def;
+    float vstab2_rud1def;
+
+    float acf_gear_deploy0;
+    float acf_gear_deploy1;
+    float acf_gear_deploy2;
+
+
+
+
 } sim_t;
