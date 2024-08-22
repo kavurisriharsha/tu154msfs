6d9efcbd2589e363236f878857e5a2322015a39f
Sriharsha Kavuri <sriharshakavuri1@gmail.com>
2024-08-22 02:31:52 -0700

    Gen logic completed

--- Tu154_Felis_open/Custom Avionics/sim.h
+++ Tu154_Felis_open/Custom Avionics/sim.h
@@ -3,4 +3,11 @@ typedef struct {
     float ENGN_N1_1;
     float ENGN_N1_2;
     int avionics_power_on;
+    int rel_genera0;
+    int rel_genera1;
+    int rel_genera2;
+    int generator_on0;
+    int generator_on1;
+    int generator_on2;
+    int APU_generator_on;
 } sim_t;
